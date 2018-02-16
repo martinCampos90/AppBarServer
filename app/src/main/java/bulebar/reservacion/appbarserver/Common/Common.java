3264030a693e8afe1ec1091529c8ebf2f2204c00
Martín Campos <mdj1690@gmail.com>
2018-02-16 02:56:10 -0600

    Agregado de seguimiento de orden por medio de google maps en detalle de orden

--- app/src/main/java/bulebar/reservacion/appbarserver/Common/Common.java
+++ app/src/main/java/bulebar/reservacion/appbarserver/Common/Common.java
@@ -4,6 +4,13 @@ package bulebar.reservacion.appbarserver.Common;
  * Created by MDJ16 on 12/02/2018.
  */
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+
+import bulebar.reservacion.appbarserver.Remote.IGeoCoordinates;
+import bulebar.reservacion.appbarserver.Remote.RetrofitClient;
 import bulebar.reservacion.appbarserver.model.Request;
 import bulebar.reservacion.appbarserver.model.User;
 
@@ -18,6 +25,8 @@ public class Common {
 
     public static  final int PICK_IMAGE_REQUEST = 71;
 
+    public static final String baseUrl = "https://maps.googleapis.com";
+
     public static String convertCodeToStatus(String code){
         if(code.equals("0"))
             return "Placed";
@@ -27,5 +36,25 @@ public class Common {
             return "Shipped";
     }
 
+    public static IGeoCoordinates getGeoCodeService(){
+        return RetrofitClient.getClient(baseUrl).create(IGeoCoordinates.class);
+    }
 
+    public static Bitmap scaleBitmap(Bitmap bitmap, int newWidth, int newHeight){
+        Bitmap scaleBitmap = Bitmap.createBitmap(newWidth,newHeight,Bitmap.Config.ARGB_8888);
+
+        float scaleX = newWidth/(float)bitmap.getWidth();
+        float scaleY = newHeight/(float)bitmap.getHeight();
+        float pivotX = 0, pivotY=0;
+
+        Matrix scaleMatrix = new Matrix();
+        scaleMatrix.setScale(scaleX,scaleY,pivotX,pivotY);
+
+        Canvas canvas =  new Canvas(scaleBitmap);
+        canvas.setMatrix(scaleMatrix);
+        canvas.drawBitmap(bitmap,0,0,new Paint(Paint.FILTER_BITMAP_FLAG));
+
+        return scaleBitmap;
+
+    }
 }
---
+++ app/src/main/java/bulebar/reservacion/appbarserver/Remote/IGeoCoordinates.java
@@ -0,0 +1,17 @@
+package bulebar.reservacion.appbarserver.Remote;
+
+/**
+ * Created by MDJ16 on 16/02/2018.
+ */
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+
+public interface IGeoCoordinates {
+    @GET("maps/api/geocode/json")
+    Call<String> getGeoCode(@Query("address") String address);
+
+    @GET("maps/api/directions/json")
+    Call<String> getDirections(@Query("origin") String origin,@Query("destination") String destination);
+}
