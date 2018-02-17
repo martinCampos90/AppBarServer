89d1284bb2428ca2f1193adfd043a469cc9b9e50
Martín Campos <mdj1690@gmail.com>
2018-02-16 20:01:52 -0600

    Agregada función de eliminar categorías junto con su comida

--- app/src/main/java/bulebar/reservacion/appbarserver/Home.java
+++ app/src/main/java/bulebar/reservacion/appbarserver/Home.java
@@ -27,8 +27,12 @@ import android.widget.Toast;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
@@ -345,6 +349,23 @@ public class Home extends AppCompatActivity
     }
 
     private void deleteCategory(String key) {
+
+        //first, we need get all foood in category
+        DatabaseReference foods = database.getReference("Foods");
+        Query foodInCategory = foods.orderByChild("menuId").equalTo(key);
+        foodInCategory.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapShot:dataSnapshot.getChildren()){
+                    postSnapShot.getRef().removeValue();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
         categories.child(key).removeValue();
         Toast.makeText(this,"Elemento borrado",Toast.LENGTH_SHORT).show();
     }
